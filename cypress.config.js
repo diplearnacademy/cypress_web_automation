cf11c2a6e722f8dc9dec87e3175e5a2e75fff53a
ingjuanfg <juan.isfg@gmail.com>
2024-07-25 18:52:17 -0500

    Se ajustan URL por ambiente y se cambia test

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
+      const environment = config.env.ENVIRONMENT || 'qa';
+
+      const envConfig = {
+        dev: "https://www.google.com",
+        qa: "https://www.saucedemo.com/",
+        pdn: "https://www.google.com.es"
+      }
+      config.baseUrl = envConfig[environment];
+      return config;
     },
   },
 });
--- cypress/e2e/login_saucedemo.cy.js
+++ cypress/e2e/login_saucedemo.cy.js
@@ -1,20 +1,19 @@
 /// <reference types="cypress">
 
 describe('Realizar Autenticacion en Saucedemo', () => {
-    it('Autenticacion exitosa', () =>{
-        cy.visit("https://www.saucedemo.com/v1/index.html")
+    it.only('Autenticacion exitosa', () =>{
+        cy.visit("/")
         cy.get('#user-name').type('standard_user')
         cy.get('[data-test="password"]').type('secret_sauce')
         cy.get('#login-button').click()
         cy.get('.product_label').should('contain.text', "Products")
     })
 
-    it.only('Autenticacion Usuario Bloqueado', () =>{
-        cy.visit("https://www.saucedemo.com/v1/index.html")
+    it('Autenticacion Usuario Bloqueado', () =>{
+        cy.visit("/")
         cy.get('#user-name').type('locked_out_user')
         cy.get('[data-test="password"]').type('secret_sauce')
         cy.get('#login-button').click()
         cy.get('.error-button').should('be.visible')
     })
-
 })
