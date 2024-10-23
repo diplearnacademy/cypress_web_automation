1576e84b1b7ce20de5b31fc4b0db634573cdbc4a
Dip Learn Academy <contacto@dlacademy.co>
2024-10-22 19:01:18 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,19 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
-
+export default defineConfig({
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-      
       const environment = config.env.ENVIRONMENT || 'pdn';
 
       const envConfig = {
         dev: "https://www.saucedemo.com/",
         qa: "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login",
         pdn: "https://teststore.automationtesting.co.uk/index.php"
-      }
+      };
+      
       config.baseUrl = envConfig[environment];
       return config;
     },
